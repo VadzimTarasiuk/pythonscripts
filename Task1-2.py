85e70e95ce33bad8262b18407cc08f3792b4deed
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-14 13:04:21 +0300

    Task2 file

---
+++ Task1-2.py
@@ -0,0 +1,10 @@
+somestring = 'abc12321cba'
+otherstring = 'this is not a polyndrome'
+stringLength = len(otherstring)
+center = stringLength//2
+for i in range(center + 1):
+    print(str(otherstring[center - i]) + ' ' + str(otherstring[center]) + ' ' + str(otherstring[center + i]))
+    if otherstring[center + i] != otherstring[center - i]:
+       print('Not a Polyndrome')
+       exit(0)
+print('Polyndrome = True')
