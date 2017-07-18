38fdea7ed2b3319691091af3fa30d18651d74642
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-18 15:29:36 +0300

    First task: Grid class

--- codewars_4.py
+++ codewars_4.py
@@ -1,7 +1,7 @@
 class Grid():
-    gridlist = []
     def __init__(self, width, height):
         self.grid = ""
+        self.gridlist = []
         for h in range(height):
             self.gridlist.append([])
             for w in range(width):
@@ -11,12 +11,21 @@ class Grid():
                 self.grid += self.gridlist[W][H]
             self.grid += "\n"
         self.grid = self.grid[:-1]
+
     def plot_point(self, x, y):
-        self.gridlist[y-1][x-1] = "X"
+        self.gridlist[x-1][y-1] = "X"
+        self.grid = ""
+        for H in range(len(self.gridlist[0])):
+            for W in range(len(self.gridlist[1])):
+                self.grid += self.gridlist[W][H]
+            self.grid += "\n"
+        self.grid = self.grid[:-1]
+
     def __repr__(self):
         print(self.grid)
+
 g = Grid(10, 10)
-g.plot_point(5,5)
+g.plot_point(0, 0)
 g.__repr__()
 
 
