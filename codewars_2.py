e343237d710687ca67eff0f35930301a19002823
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-18 12:32:04 +0300

    First task: Sum raws

--- codewars_2.py
+++ codewars_2.py
@@ -1,4 +1,22 @@
+n = 37
 def show_sequence(n):
-    result = n
-    pass
+    """
+    Sum function, creates raw of 0+1+2+3+..+n
+    :param n:
+    :return result of string:
+    """
+    if n < 0:
+        return("%d<0" % n)
+    elif n == 0:
+        return("0=0")
+    elif n > 0:
+        result = "0"
+        x = 1
+        sum = 0
+        while x <= n:
+            result += "+%d" % x
+            sum += x
+            x += 1
+        result += " = %d" % sum
     return result
+print(show_sequence(n))
