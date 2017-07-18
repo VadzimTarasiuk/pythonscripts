670085bd85ce3be8b467d8df780e16b914c4c54d
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-18 16:11:51 +0300

    First task: some Weird calculations

---
+++ codewars_5.py
@@ -0,0 +1,18 @@
+n = 17
+def fcn (n):
+    """
+    Given u0 = 1, u1 = 2 and the relation 6unun+1-5unun+2+un+1un+2 = 0
+    calculate un for any integer n >= 0.
+    fcn(n) returns un: fcn(17) -> 131072, fcn(21) -> 2097152
+    :param n:
+    :return: Nvalue of integer
+    """
+    U = [1, 2]
+    U.append(0)
+    U[2] = (6*U[0]*U[1])/(5*U[0] - U[1])
+    for x in range(2, n+2):
+        U.append(0)
+        U[x] = (6*U[x-2]*U[x-1])//(5*U[x-2] - U[x-1])
+    Nvalue = U[n]
+    return Nvalue
+print(fcn(n))
